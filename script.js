b3451ddfb0d188005be27e1a4338f1a578d6082f
Nishant Gautam <eddienishant@gmail.com>
2020-09-19 13:07:35 +0530

    added some HTML structure

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(eval('2'+'3'));
+
+// It evaluates the result, we need to pass the string it will do evalution for us.
+
+// we can pass varuable as well, and use eval to evaluate the expression as wellclear
+
+var operand1= 2;;
+var operand2 = 4
+var operator = "*";
+var result = eval(operand1 + " " + operator + " " + operand2)
+console.log(result);
