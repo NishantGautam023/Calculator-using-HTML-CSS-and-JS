4c3f595bbb6cf1cbcd88987d63242127e4cd3a8b
Nishant Gautam <eddienishant@gmail.com>
2020-09-20 19:31:12 +0530

    updated html

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var button = document.getElementsByClassName('button');
 var display = document.getElementById('display');
 var operand1 = 0;
 var operand2 = null;
-var operator3 = null;
+var operator = null;
 
 // Handling clicks on all the buttons.
 for (var i =0; i< button.length; i++ ) {
@@ -10,9 +10,13 @@ for (var i =0; i< button.length; i++ ) {
     button[i].addEventListener('click', function() {
         // all the calculator logic is within this function
         var value= this.getAttribute('data-value');
-        if (value == "+") {
+        if (value == "+") { // if an operator is clicked
             operator = "+";
             operand1 = parseFloat(display.textContent)
+        } else if(value == '=') {
+            operand2 = parseFloat(display.textContent);
+            // use eval to ge result
+            // show result on display
         }
     });
 } 
